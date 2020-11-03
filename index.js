a598161bfaa9c0aad549ebff9d9f4b5d2553c850
wonderlexicon <nh756@nyu.edu>
2020-11-03 11:10:43 -0500

    set up working server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-   
+let express = require('express');
+let app = express();
+app.use("/", express.static("."));
+
+let http = require('http');
+let server = http.createServer(app);
+let port = process.env.PORT ||3000;
+server.listen(port,() =>{
+    console.log("server listening");
+});
